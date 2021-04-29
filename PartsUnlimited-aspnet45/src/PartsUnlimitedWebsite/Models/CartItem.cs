3015fd8971d9d6c763d3177e83c7f4fc61c939be
Yeray <yeray.ariza.morales@avanade.com>
2021-04-29 14:04:01 +0200

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //Mi primerito cambio
+        //Mi segundito cambio
         [Key]
         public int CartItemId { get; set; }
 
